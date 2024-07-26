5efc5f06f8b7ef85f7e4b025fb97949b1abe1104
Adhithiyan R <adhithiyan.21it@sonatech.ac.in>
2024-07-26 23:46:53 +0530

    Update Sidebar.js

--- app/Components/sidebar/Sidebar.js
+++ app/Components/sidebar/Sidebar.js
@@ -10,7 +10,7 @@ const Sidebar = () => {
    <>
     <aside className={toggle ? "aside show-menu" : "aside"}>
     <a href='#home' className='nav__logo'>
-    <img src='https://i.ibb.co/w7NMgzw/User-Image.jpg' alt='sideLogo' />
+    <img src='https://i.ibb.co/j6YFgn7/ADHITHIYAN.jpg' alt='sideLogo' />
     </a>
 
     <nav className='nav'>
